357dd858a1682d2b2268cf7ef65af60c57712234
Park, Hyung Soon <pcktmnstr90@gmail.com>
2015-06-25 15:55:14 +0900

    Refactoring

--- find kth number/kthNumber.c
+++ find kth number/kthNumber.c
@@ -5,15 +5,24 @@ Kth number
 #include <stdio.h>
 #include <stdlib.h>
 
-int findK(int b);
-int square(int a, int c);
+
+int findK(int k);
+
+int exponentation(int base, int exp)    //편의상 만든 제곱함수
+{
+	int result = 1;
+
+	while(exp--)
+		result *= base;
+
+	return result;
+}
 
 void main()
 {
 	FILE *in_file;
 	int i;
-	int no_cases, no_data;
-	int k;
+	int no_cases, inputData;
 
 	in_file = fopen("input.txt", "r");
 	if(in_file == NULL) exit(1);
@@ -22,91 +31,66 @@ void main()
 
 	for(i=0; i<no_cases; i++)
 	{
-		fscanf(in_file, "%d", &no_data);
-
-		k = findK(no_data);
-		printf("%d\n", k);
+		fscanf(in_file, "%d", &inputData);
+		(inputData < 10 ? printf("%d\n", inputData) : printf("%d\n", findK(inputData)));
 	}
 	fclose(in_file);
 }
 
 
-int findK(int a)
+int findK(int k)
 {
-	int x = 20;
-	int y = 9;
+	int x = 20, y = 9; // 가상의 표의 크기
 	int z, temp, position, row, column;
-	int k;
+	int kthNumber;
 
-	if(a<10)       // k번째수가 포함되는 숫자가 몇자리 수인지 알아보는 과정
+	// k번째수가 포함되는 숫자가 몇자리 수인지 알아보는 과정	
+	k = k - 9; // 1~9 제외
+	do						// 자리수를 넘어갈 경우 다음 표를 찾는다
+	{						// 표는 해당 자리수의 처음숫자부터 마지막 숫자까지 늘어놓은 표이다.
+		temp = k;			// 예를 들면 두자리수 표라면 10부터 99까지 늘어놓은 표이다.
+		k = k - (x * y);		// 각 행은 10의 자리수로 끊는다.
+		x = (x + 10);
+		y = y * 10;
+	}
+	while(k > 0);
+
+	k = temp; 
+
+	z = (x / 10) - 1;		// z에 위에서 몇자리 수인지 구한 것을 대입
+
+	position = k % z;		// k번쨰 수가 포함된 숫자에서 k번째숫자의 위치-왼쪽에서부터 센다 (ex. 104 에서 k번째수가 1 이라면 position은 1, 4라면 position은3)
+
+	column = k % (z * 10);	// 가상의 열  (ex. z가 3일 경우 100에서 109까지 나열하면 30개의 열이 생긴다)
+	row = k / (z * 10);		// 가상의 행  (ex. z가 2일 경우 가로20 세로9 (10~90)의 가상의 표를 만들 수 있다)
+
+	if(column == 0)			// 마지막 열의 경우는 10으로 나누어 떨어져서 다음열로 넘어가게 되어있으므로 재 설정해준다.
 	{
-		k = a;
-		return k;
+		column = z * 10;      
+		row = row - 1;
 	}
 
-	else
+	if(position == 0 && column != 0)		// k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 마지막 자리(1의자리)일 경우 예) 103에서 3이 k번째일때
+		kthNumber = (column / z) - 1;
+
+	else if(position == 0 && column == 0)	// 위 조건으로는 마지막 자리중 열의 마지막인 경우가 안되므로 재설정
+		kthNumber = 9;       
+
+	else if(position == 1)					// k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 첫번째 자리일 경우
+		kthNumber = ((row / exponentation(10, (z-2))) % 9) +1;
+
+	else									// k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 중간부분일 경우
 	{
-		a = a-9;
-		do
-		{
-			temp = a;
-			a= a-(x*y);
-			x=(x+10);
-			y=y*10;
+		temp = (z - (position + 1));
+		while(temp > 0)						// 중간에서 가장 오른쪽에 있을때부터 왼쪽으로 한번씩 갈때마다
+		{									// 10으로 나누어 주어야 10으로 나누었을때 나머지가 k번째 수가된다.
+			row = row / 10;
+			temp--;
 		}
-		while(a>0);      
-
-		a = temp; 
-
-		z = (x/10)-1;      //z에 위에서 몇자리 수인지 구한 것을 대입
-
-		position = a % z;     //k번쨰 수가 포함된 숫자에서 k번째숫자의 위치-왼쪽에서부터 센다 (ex. 104 에서 k번째수가 1 이라면 position은 1, 4라면 position은3)
-
-		column = a % (z*10);  //가상의 열  (ex. z가 3일 경우 100에서 109까지 나열하면 30개의 열이 생긴다)
-		row = a / (z*10);     //가상의 행  (ex. z가 2일 경우 가로20 세로9 (10~90)의 가상의 표를 만들 수 있다)
-
-		if(column == 0)      //마지막 열의 경우는 10으로 나누어 떨어져서 다음열로 넘어가게 되어있으므로 재 설정해준다.
-		{
-			column = z*10;      
-			row = row - 1;
-		}
-
-		if(position == 0 && column != 0)  //k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 마지막 자리(1의자리)일 경우
-			k = (column / z) - 1;
-		else if(position == 0 && column == 0) //위 조건으로는 마지막 자리중 열의 마지막인 경우가 안되므로 재설정
-			k = 9;       
-
-		else if(position == 1)    //k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 첫번째 자리일 경우
-		{ 
-			k = ((row / square(10, (z-2))) % 9) +1;
-		}
-
-		else        //k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 중간부분일 경우
-		{
-			temp = (z-(position + 1));
-			while(temp > 0)     //중간에서 가장 오른쪽에 있을때부터 왼쪽으로 한번씩 갈때마다 10으로 나누어 주어야 10으로 나누었을때 나머지가 k번째 수가된다.
-			{
-				row = row / 10;
-				temp--;
-			}
-
-			row = row % 10;
-			k = row;
-		}
-
-		return k;
+		row = row % 10;
+		kthNumber = row;
 	}
+
+	return kthNumber;
 }
 
-int square(int a, int b)    //편의상 만든 제곱함수
-{
-	int i;
-	int c = 1;
-
-	for(i = 1; i <= b; i++)
-	{
-		c *= a;
-	}
-
-	return c;
-}
---
+++ find kth number/kthNumberOld.c
@@ -0,0 +1,112 @@
+﻿/*
+Kth number
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int findK(int b);
+int square(int a, int c);
+
+void main()
+{
+	FILE *in_file;
+	int i;
+	int no_cases, no_data;
+	int k;
+
+	in_file = fopen("input.txt", "r");
+	if(in_file == NULL) exit(1);
+
+	fscanf(in_file, "%d", &no_cases);
+
+	for(i=0; i<no_cases; i++)
+	{
+		fscanf(in_file, "%d", &no_data);
+
+		k = findK(no_data);
+		printf("%d\n", k);
+	}
+	fclose(in_file);
+}
+
+
+int findK(int a)
+{
+	int x = 20;
+	int y = 9;
+	int z, temp, position, row, column;
+	int k;
+
+	if(a<10)       // k번째수가 포함되는 숫자가 몇자리 수인지 알아보는 과정
+	{
+		k = a;
+		return k;
+	}
+
+	else
+	{
+		a = a-9;
+		do
+		{
+			temp = a;
+			a= a-(x*y);
+			x=(x+10);
+			y=y*10;
+		}
+		while(a>0);      
+
+		a = temp; 
+
+		z = (x/10)-1;      //z에 위에서 몇자리 수인지 구한 것을 대입
+
+		position = a % z;     //k번쨰 수가 포함된 숫자에서 k번째숫자의 위치-왼쪽에서부터 센다 (ex. 104 에서 k번째수가 1 이라면 position은 1, 4라면 position은3)
+
+		column = a % (z*10);  //가상의 열  (ex. z가 3일 경우 100에서 109까지 나열하면 30개의 열이 생긴다)
+		row = a / (z*10);     //가상의 행  (ex. z가 2일 경우 가로20 세로9 (10~90)의 가상의 표를 만들 수 있다)
+
+		if(column == 0)      //마지막 열의 경우는 10으로 나누어 떨어져서 다음열로 넘어가게 되어있으므로 재 설정해준다.
+		{
+			column = z*10;      
+			row = row - 1;
+		}
+
+		if(position == 0 && column != 0)  //k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 마지막 자리(1의자리)일 경우
+			k = (column / z) - 1;
+		else if(position == 0 && column == 0) //위 조건으로는 마지막 자리중 열의 마지막인 경우가 안되므로 재설정
+			k = 9;       
+
+		else if(position == 1)    //k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 첫번째 자리일 경우
+		{ 
+			k = ((row / square(10, (z-2))) % 9) +1;
+		}
+
+		else        //k번째 숫자가 포함된 숫자에서 k번째 숫자의 위치가 중간부분일 경우
+		{
+			temp = (z-(position + 1));
+			while(temp > 0)     //중간에서 가장 오른쪽에 있을때부터 왼쪽으로 한번씩 갈때마다 10으로 나누어 주어야 10으로 나누었을때 나머지가 k번째 수가된다.
+			{
+				row = row / 10;
+				temp--;
+			}
+
+			row = row % 10;
+			k = row;
+		}
+
+		return k;
+	}
+}
+
+int square(int a, int b)    //편의상 만든 제곱함수
+{
+	int i;
+	int c = 1;
+
+	for(i = 1; i <= b; i++)
+	{
+		c *= a;
+	}
+
+	return c;
+}
